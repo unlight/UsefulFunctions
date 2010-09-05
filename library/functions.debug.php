24106821d1cc695ebc7e66e8ed2f62b7a651ae06
unknown <unlights@gmail.com>
2010-09-05 23:19:54 +0400

    Debugs for plugin manager...

--- library/functions.debug.php
+++ library/functions.debug.php
@@ -26,3 +26,56 @@ if(!function_exists('d')) {
 		if($bExit) exit();
 	}
 }
+
+function dplg($PluginFunction){
+	
+	if(!IsSet($_GET['d'])) return;
+	if(in_array($PluginFunction, array('gdn_locale_beforetranslate_handler', 'base_beforetranslate_handler'))) return;
+	
+	$Args = func_get_args();
+	if(!defined('DPLG_STYLE')){
+		define('DPLG_STYLE', 1);
+		echo "<style type='text/css'>em.d{border:1px dashed black;font-size:13px;font-family:Tahoma;display:inline-block;padding:0 2;margin:1;}</style>";
+	}
+
+	printf("\n<em class='d'>%s</em>", $PluginFunction);
+	
+	//@ob_end_flush();
+}
+
+
+function dplg_(){
+	
+	if(!IsSet($_GET['d'])) return;
+	
+	$Args = func_get_args();
+	if(!defined('DPLG_STYLE')){
+		define('DPLG_STYLE', 1);
+		echo "<style type='text/css'>em.d{border:1px dashed black;font-size:13px;font-family:Tahoma;display:inline-block;padding:0 2;margin:1;}</style>";
+	}
+	$Args[0] = "\n<em class='d'>$Args[0]</em>";
+	for($i = 1; $i < Count($Args); $i++){
+		$S =& $Args[$i];
+		if(Is_String($S)){
+			$S = ucfirst($S);
+			$LowerKeywords = array('module', 'model', 'add', 'query', 'controller', 'before', 'after', 'plugin', 'render', 'comment', 'count', 'update');
+			$UpperKeywords = array_map('ucfirst', $LowerKeywords);
+			$S = preg_replace('/(^|_\w)/e', 'strtoupper("$1")', $S);
+			$S = str_replace($LowerKeywords, $UpperKeywords, $S);
+		}elseif(Is_Array($S)){
+			if(Count($S) == 0) $S = "";
+			elseif(Count($S) >= 1){
+				$S = " (" . implode(', ', Array_Keys($S)) . ")";
+			}
+		}
+	}
+	call_user_func_array('printf', $Args);
+	
+	return;
+
+	$LogMessage = call_user_func_array('sprintf', $Args);
+	$LogMessage = strip_tags($LogMessage);
+	$Info = "POST @ " . (bool)$_POST;
+	error_log("$LogMessage ($Info)", 3, 'dplg.log');
+	
+}
