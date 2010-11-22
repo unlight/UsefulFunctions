fedfd350e746bd4af31a05a6524a0d817b88d569
Ronnie <unlights@gmail.com>
2010-11-22 20:24:29 +0300

    IntervalSeconds()

--- library/functions.time.php
+++ library/functions.time.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!function_exists('Now')) {
+if (!function_exists('Now')) {
 	function Now(){
 		return microtime(True);
 	}
@@ -9,8 +9,18 @@ if(!function_exists('Now')) {
 /**
 * Get the number of seconds since the current year
 */
-if(!function_exists('YearSeconds')) {
+if (!function_exists('YearSeconds')) {
 	function YearSeconds(){
 		return (time() - strtotime('1 Jan'));
+	}
+}
+
+/**
+* String interval to seconds
+* Example: IntervalSeconds('2 minutes') => 120
+*/
+if (!function_exists('IntervalSeconds')) {
+	function IntervalSeconds($StringDate) {
+		return strtotime($StringDate, 0);
 	}
 }
