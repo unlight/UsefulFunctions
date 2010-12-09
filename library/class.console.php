9deb5be474460dab03544cdde9cc47d85c18176b
Ronnie <unlights@gmail.com>
2010-12-09 11:19:03 +0300

    Color console messages

--- library/class.console.php
+++ library/class.console.php
@@ -66,14 +66,63 @@ class Console extends Gdn_Pluggable {
 		return $Result;
 	}
 	
-	public static function Colorize($S) {
-		$bColorSupport = self::CheckColorSupport();
-		if ($bColorSupport) {
-			// colorize
-		} else {
-			// strip color tokens
+	static protected $ForegroundColor = array(
+		'Black' => 30,
+		'Blue' => 34,
+		'Green' => 32,
+		'Cyan' => 36,
+		'Red' => 31,
+		'Purple' => 35,
+		'Brown' => 33,
+		'Yellow' => 33,
+		'White' => 37
+	);
+
+	public static function GetColorCode($Color = Null) {
+		if ($Color === Null || !array_key_exists($Color, self::$ForegroundColor)) return "\033[0m";
+		return "\033[" . self::$ForegroundColor[$Color] . 'm';
+	}
+	
+	public static function ColorizeForeground($ColorToken = '') { // ColorToken = ^1
+		$N = substr($ColorToken, 1, 1);
+		if ($N === '') return self::GetColorCode();
+		switch ($N) {
+			case 0: return self::GetColorCode('White');
+			case 1: return self::GetColorCode('Red');
+			case 2: return self::GetColorCode('Green');
+			case 3: return self::GetColorCode('Yellow');
+			case 4: return self::GetColorCode('Blue');
+			case 5: return self::GetColorCode('Turq');
+			case 6: return self::GetColorCode('Pink');
+			case 7: return self::GetColorCode('LightRed');
+			case 8: return self::GetColorCode('Grey');
+			case 9: return self::GetColorCode('GreyWhite');
+			default: break;
 		}
-		return $S;
+		return self::GetColorCode(); // reset
+	}
+	
+	public static function ColorizeMessage($PaintString) { // ex- Paint
+		$bColorSupport = self::CheckColorSupport();
+		if (!$bColorSupport) return preg_replace('/\^\d/', '', $PaintString);
+		$OutString = '';
+		$Pos = strpos($PaintString, '^');
+		while ($Pos > -1) {
+			if ($Pos > 0) {
+				$OutString .= substr($PaintString, 0, $Pos);
+				$PaintString = substr($PaintString, $Pos);
+				$Pos = 0;
+			}
+			$ColorToken = substr($PaintString, $Pos, 2);
+			$N = strlen($PaintString) - 2;
+			if ($N > 0) $PaintString = substr($PaintString, -$N);
+			else $PaintString = '';
+			$OutString .= self::ColorizeForeground($ColorToken);
+			$Pos = strpos($PaintString, '^');
+		}
+		if ($PaintString != '') $OutString .= $PaintString;
+		$OutString .= self::GetColorCode();
+		return $OutString;
 	}
 	
 	public static function Message() {
@@ -86,7 +135,7 @@ class Console extends Gdn_Pluggable {
 		if($Count != Count($Args) - 1) $Message = str_replace('%', '%%', $Message);
 		$Message = call_user_func_array('sprintf', $Args);
 		if($Encoding && $Encoding != 'utf-8') $Message = mb_convert_encoding($Message, $Encoding, 'utf-8');
-		$S = self::TimeSeconds() . ' -!- ' . $Message;
+		$S = self::TimeSeconds() . ' -!- ' . self::ColorizeMessage($Message);
 		if (substr($S, -1, 1) != "\n") $S .= "\n";
 		fwrite(STDOUT, $S);
 	}
@@ -121,9 +170,9 @@ class Console extends Gdn_Pluggable {
 	public static function Wait($Seconds = 1, $bDrawDots = True) {
 		fwrite(STDOUT, self::TimeSeconds() . ' -!- Waiting.');
 		$Seconds = Clamp((int)$Seconds, 1, 3600); // 1 hour max
-		for($i = 0; $i < $Seconds; $i++){
+		for ($i = 0; $i < $Seconds; $i++) {
 			sleep(1);
-			if($bDrawDots) fwrite(STDOUT, '.');
+			if ($bDrawDots) fwrite(STDOUT, '.');
 		}
 		fwrite(STDOUT, "\n");
 	}
