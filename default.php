298ca0c1c077bcc0bbb094b12c071cbd1d50c940
Ronnie <unlights@gmail.com>
2010-12-18 21:14:15 +0300

    New function

--- default.php
+++ default.php
@@ -4,7 +4,7 @@ $PluginInfo['PluginUtils'] = array(
 	'Name' => 'Plugin utils',
 	'Description' => 'Useful functions for plugin and application developers.',
 	'RequiredApplications' => array('Dashboard' => '>=2.0.13'),
-	'Version' => '2.2.8.b58',
+	'Version' => '2.2.9.b59',
 	'Author' => 'Vanilla Fan'
 );
 
--- library/functions.dom.php
+++ library/functions.dom.php
@@ -1,8 +1,38 @@
 <?php
 
-function LoadPhpQuery() {
-	//if(!class_exists('PhpQuery')) require_once dirname(__FILE__).DS.'vendors' . DS . 'phpQuery.php';
-	if (!function_exists('Pq')) require_once PLUGINUTILS_VENDORS . DS . 'phpQuery.php';
+/**
+* Parse a selector of the form #foo.bar.baz into constituent ID and classes.
+* An array argument will be returned unchanged.
+* php-helpers by Jason Frame [jason@onehackoranother.com]
+*/
+
+if (!function_exists('SelectorAttribute')) {
+	function SelectorAttribute($Selector, &$Attributes = False) {
+		$Return = array();
+		preg_match('/^(#([\w-]+))?((\.[\w-]+)*)$/', $Selector, $Matches);
+        if (!empty($Matches[2])) $Return['id'] = $Matches[2];
+        if (!empty($Matches[3])) $Return['class'] = trim(str_replace('.', ' ', $Matches[3]));
+		if (is_array($Attributes)) $Return = array_merge($Attributes, $Return);
+		return $Return;
+	}
+}
+
+
+/*function parse_simple_selector($s) {
+    if (!is_array($s)) {
+        preg_match('/^(#([\w-]+))?((\.[\w-]+)*)$/', $s, $matches);
+        $s = array();
+        if (!empty($matches[2])) $s['id'] = $matches[2];
+        if (!empty($matches[3])) $s['class'] = trim(str_replace('.', ' ', $matches[3]));
+    }
+    return $s;
+}*/
+
+if (!function_exists('LoadPhpQuery')) {
+	function LoadPhpQuery() {
+		//if(!class_exists('PhpQuery')) require_once dirname(__FILE__).DS.'vendors' . DS . 'phpQuery.php';
+		if (!function_exists('Pq')) require_once PLUGINUTILS_VENDORS . DS . 'phpQuery.php';
+	}
 }
 
 /**
@@ -14,18 +44,20 @@ function LoadPhpQuery() {
 * More information: http://code.google.com/p/phpquery/
 */ 
 
-function PqDocument($Document, $Options = False) {
-	if (!function_exists('Pq')) require_once PLUGINUTILS_VENDORS . DS . 'phpQuery.php';
-	if (strpos($Document, '<') === False) {
-		if (is_file($Document) || (substr($Document, 0, 4) == 'http')) {
-			$Document = file_get_contents($Document);
+if (!function_exists('PqDocument')) {
+	function PqDocument($Document, $Options = False) {
+		if (!function_exists('Pq')) require_once PLUGINUTILS_VENDORS . DS . 'phpQuery.php';
+		if (strpos($Document, '<') === False) {
+			if (is_file($Document) || (substr($Document, 0, 7) == 'http://')) {
+				$Document = file_get_contents($Document);
+			}
 		}
+		if (GetValue('ConvertEncoding', $Options)) $Document = ConvertEncoding($Document);
+		
+		if (GetValue('FixHtml', $Options, True)) {
+			$HtmlFormatter = Gdn::Factory('HtmlFormatter');
+			if ($HtmlFormatter) $Document = $HtmlFormatter->Format($Document);
+		}
+		return phpQuery::newDocumentXHTML($Document);
 	}
-	if (GetValue('ConvertEncoding', $Options)) $Document = ConvertEncoding($Document);
-	
-	if (GetValue('FixHtml', $Options, True)) {
-		$HtmlFormatter = Gdn::Factory('HtmlFormatter');
-		if ($HtmlFormatter) $Document = $HtmlFormatter->Format($Document);
-	}
-	return phpQuery::newDocumentXHTML($Document);
 }
