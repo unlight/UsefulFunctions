47381cee7d49beb87e59b76c0465cca0e11e13ab
Ronnie <unlights@gmail.com>
2011-04-22 13:32:57 +0400

    undefined constant

--- bin/tick.php
+++ bin/tick.php
@@ -25,7 +25,7 @@ if ($InTick === TRUE) {
 SaveToConfig('Plugins.UsefulFunctions.InTick', TRUE);
 
 ini_set('memory_limit', '256M');
-require UsefulFunctions_LIBRARY . DS .  'class.tick.php';
+require USEFULFUNCTIONS_LIBRARY . DS .  'class.tick.php';
 
 $Handler = new Tick();
 
