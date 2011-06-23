6ce57825db4bbe3beb586836ae35c20fe73a589c
S <unlights@gmail.com>
2011-06-24 00:11:31 +0400

    ValidateDnsEmail() docs, use explode to split string

--- library/functions.validate.php
+++ library/functions.validate.php
@@ -1,18 +1,17 @@
 <?php
 
-
 /**
-* Validate email using getmxrr() function
+* Validate email using getmxrr() function.
+* 
+* @param string $Value, email which need to be validated.
+* @return bool $Result.
 */
-
-if(!function_exists('ValidateDnsEmail')) {
+if (!function_exists('ValidateDnsEmail')) {
 	function ValidateDnsEmail($Value, $Field = '') {
-		if($Value === '') return True; // + required
+		if ($Value === NULL) return True; // + required
 		$Result = ValidateEmail($Value, $Field);
-		if($Result !== True) return 'ValidateEmail';
-		$EmailArray = SplitString($Value, '@');
-		$User = ArrayValue(0, $EmailArray);
-		$Host = ArrayValue(1, $EmailArray);
+		if ($Result !== True) return 'ValidateEmail';
+		list($User, $Host) = explode('@', $Value);
 		$MxHosts = array();
 		$Result = getmxrr($Host, $MxHosts);
 		return $Result;
