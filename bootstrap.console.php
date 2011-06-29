c10cba09c757dca2124889fa3f60d8af04b798ea
S <unlights@gmail.com>
2011-06-29 10:50:27 +0400

    MemoryLimit

--- bootstrap.console.php
+++ bootstrap.console.php
@@ -1,25 +1,27 @@
 <?php
 error_reporting(E_ALL);
-ini_set('html_errors', 'Off');
-ini_set('display_errors', 'On');
-ini_set('track_errors', 'On');
+ini_set('html_errors', 0);
+ini_set('display_errors', 1);
+ini_set('track_errors', 1);
 
 if (!defined('APPLICATION')) define('APPLICATION', 'Garden');
 if (!defined('APPLICATION_VERSION')) define('APPLICATION_VERSION', '1.0');
 if (!defined('DS')) define('DS', '/');
 
-chdir(dirname(__FILE__).DS.'..'.DS.'..');
+chdir(dirname(__FILE__).'/../..');
 if (!defined('PATH_ROOT')) define('PATH_ROOT', realpath('.'));
 
-require_once PATH_ROOT.DS.'bootstrap.php';
-if (!defined('USEFULFUNCTIONS_LIBRARY')) define('USEFULFUNCTIONS_LIBRARY', dirname(__FILE__).DS.'library');
+require_once PATH_ROOT.'/bootstrap.php';
+if (!defined('USEFULFUNCTIONS_LIBRARY')) define('USEFULFUNCTIONS_LIBRARY', dirname(__FILE__).'/library');
 require_once USEFULFUNCTIONS_LIBRARY.'/class.console.php';
 
 if(!Console::Check() && C('Plugins.UsefulFunctions.Console.Check')) die('Please, run script from command line!');
 
-if (PHP_SAPI == 'cli') $_GET['DeliveryType'] = 'VIEW';
-
-@ob_end_clean();
+if (PHP_SAPI == 'cli') {
+	ini_set('memory_limit', C('Plugins.UsefulFunctions.Console.MemoryLimit', '256M'));
+	$_GET['DeliveryType'] = 'VIEW';
+}
 
 $Host = GetValue(1, explode('//', Gdn::Config('Garden.Domain')));
 Gdn::Request()->RequestHost($Host);
+
