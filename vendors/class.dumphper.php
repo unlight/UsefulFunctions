94222281ca7cf177cdb7f7977d776da6a1f02c47
S <s91630277@gmail.com>
2011-09-03 17:37:18 +0400

    Fixed drawScalar() function '0' dispplayed as empty string

--- vendors/class.dumphper.php
+++ vendors/class.dumphper.php
@@ -1,251 +1,258 @@
 <?php
 /**
- * Impored var_dump for PHP 5.2.3++
- * @author Dmitry "widowmaker" Khavilo
- * @email wm .dot. morgun .at. gmail .dot. com
- **/
+* Impored var_dump for PHP 5.2.3++
+* @author Dmitry "widowmaker" Khavilo
+* @email wm .dot. morgun .at. gmail .dot. com
+**/
+
+// Fixed by S
 
 class Dumphper
 {
-  /** config **/
-  
-  static $encoding = 'UTF-8';  /** Text encoding, needed to escape stings **/
-  static $escape_keys = false; /** Should array keys be escaped (slow) **/
-  static $max_showw_depth = 8; /** Defines how many nested levels will be expanded by default **/
-  
-  /** don't touch **/
-  static $objects = array();
-  static $calls = 0;
-  static $depth = 0;
-  
-  static function dump(&$source)
-  {
-    self::$depth = 0;
-    self::drawStyles();
-    //$s = microtime(true);
-    //self::$objects = array();
-    self::$calls++;
-    echo '<div class="dumphper">';
-    self::_dump($source);
-    echo '</div>';
-    //echo round(microtime(true) - $s,6)*1000 . 's';
-  }
-  
-  static function _dump(&$source, &$parents = array())
-  {
-    self::$depth++;
-    switch(gettype($source))
-    {
-      case 'array':
-        self::drawArray($source, $parents);
-        break;
-      case 'object':
-        self::drawObject($source, $parents);
-        break;
-      case 'NULL':
-        self::drawNULL();
-        break;
-      case 'boolean':
-        self::drawBoolean($source);
-        break;
-      case 'resource':
-        self::drawResource($source);
-        break;
-      default:
-        self::drawScalar($source);
-        break;
-    }
-    self::$depth--;
-  }
-  
-  static function escape(&$source)
-  {
-    return ( is_string($source) ? ( $source ? htmlentities($source, ENT_QUOTES, self::$encoding) : '&nbsp;') : $source );
-  }
-  
-  static function drawScalar(&$source, &$escape = true)
-  {
-    self::drawValue($escape ? self::escape($source) : $source, gettype($source));
-  }
-  
-  static function drawNULL()
-  {
-    self::drawValue('NULL', 'null');
-  }
-  
-  static function drawResource(&$source)
-  {
-    self::drawValue(get_resource_type($source) . ' ' . strtolower((string)$source) , 'resource');
-  }
+	/** config **/
+	
+	static $encoding = 'UTF-8';  /** Text encoding, needed to escape stings **/
+	static $escape_keys = false; /** Should array keys be escaped (slow) **/
+	static $max_showw_depth = 8; /** Defines how many nested levels will be expanded by default **/
+	
+	/** don't touch **/
+	static $objects = array();
+	static $calls = 0;
+	static $depth = 0;
+	
+	static function dump(&$source)
+	{
+		self::$depth = 0;
+		self::drawStyles();
+		//$s = microtime(true);
+		//self::$objects = array();
+		self::$calls++;
+		echo '<div class="dumphper">';
+		self::_dump($source);
+		echo '</div>';
+		//echo round(microtime(true) - $s,6)*1000 . 's';
+	}
+	
+	static function _dump(&$source, &$parents = array())
+	{
+		self::$depth++;
+		switch(gettype($source))
+		{
+			case 'array':
+				self::drawArray($source, $parents);
+				break;
+			case 'object':
+				self::drawObject($source, $parents);
+				break;
+			case 'NULL':
+				self::drawNULL();
+				break;
+			case 'boolean':
+				self::drawBoolean($source);
+				break;
+			case 'resource':
+				self::drawResource($source);
+				break;
+			default:
+				self::drawScalar($source);
+				break;
+		}
+		self::$depth--;
+	}
+	
+	static function escape(&$source)
+	{
+		return ( is_string($source) ? ( $source ? htmlentities($source, ENT_QUOTES, self::$encoding) : '&nbsp;') : $source );
+	}
+	
+	static function drawScalar(&$source, &$escape = true)
+	{
+        if ($source === '0') $escape = False;
+		self::drawValue($escape ? self::escape($source) : $source, gettype($source));
+	}
+	
+	static function drawNULL()
+	{
+		self::drawValue('NULL', 'null');
+	}
+	
+	static function drawResource(&$source)
+	{
+		self::drawValue(get_resource_type($source) . ' ' . strtolower((string)$source) , 'resource');
+	}
 
-  static function drawBoolean(&$source)
-  {
-    self::drawValue($source ? 'true' : 'false', 'boolean');
-  }
+	static function drawBoolean(&$source)
+	{
+		self::drawValue($source ? 'true' : 'false', 'boolean');
+	}
 
-  static function drawArray(&$source, &$parents)
-  {
-    if (self::isRecursiveArray($source, $parents))
-    {
-      self::drawValue('RECURSION', 'recursion');
-    }
-    else
-    {
-      $c = count($source);
-      echo '<div class="dumphper-container' . (self::$depth <= self::$max_showw_depth ? '' : ' dumphper-closed') . '">';
-      self::drawHeader( $c > 0 ? '<a class="dumphper-toggler" href="javascript:;" onclick="dumphper_toggle(this);">Array (' . $c . ')</a>' : 'empty Array ', 'array');
-      if ( $c > 0 )
-      {
-        echo '<table class="dumphper-table" cellspacing="0" cellpadding="0">';
-        foreach ($source as $key => &$value)
-        {
-          echo '<tr><td class="dumphper-key-array">';
-          self::drawScalar($key, self::$escape_keys);
-          echo '</td><td>';
-          self::_dump($value, $parents);
-          echo '</td></tr>';
-        }
-        echo '</table>';
-      }
-      echo '</div>';
-    }
-  }
-  
-  static function getObjectId(&$source)
-  {
-    $class = get_class($source);
-    if ( !isset(self::$objects[$class]) )
-      self::$objects[$class] = array();
-    foreach (self::$objects[$class] as $id => $obj)
-      if ( $source === $obj )
-        return $id;
-    self::$objects[$class][] = &$source;
-    return count(self::$objects[$class])-1;
-  }
-  
-  static function drawObject(&$source, &$parents)
-  {
-    $className = get_class($source);
-    $object_index = self::getObjectId($source);
-    $object_id = 'dhph_' . $className . '_' . self::$calls . '_' . $object_index;
+	static function drawArray(&$source, &$parents)
+	{
+		if (self::isRecursiveArray($source, $parents))
+		{
+			self::drawValue('RECURSION', 'recursion');
+		}
+		else
+		{
+			$c = count($source);
+			echo '<div class="dumphper-container' . (self::$depth <= self::$max_showw_depth ? '' : ' dumphper-closed') . '">';
+			self::drawHeader( $c > 0 ? '<a class="dumphper-toggler" href="javascript:;" onclick="dumphper_toggle(this);">Array (' . $c . ')</a>' : 'empty Array ', 'array');
+			if ( $c > 0 )
+			{
+				echo '<table class="dumphper-table" cellspacing="0" cellpadding="0">';
+				foreach ($source as $key => &$value)
+				{
+					echo '<tr><td class="dumphper-key-array">';
+					self::drawScalar($key, self::$escape_keys);
+					echo '</td><td>';
+					self::_dump($value, $parents);
+					echo '</td></tr>';
+				}
+				echo '</table>';
+			}
+			echo '</div>';
+		}
+	}
+	
+	static function getObjectId(&$source)
+	{
+		$class = get_class($source);
+		if ( !isset(self::$objects[$class]) )
+			self::$objects[$class] = array();
+		foreach (self::$objects[$class] as $id => $obj)
+			if ( $source === $obj )
+				return $id;
+		self::$objects[$class][] = &$source;
+		return count(self::$objects[$class])-1;
+	}
+	
+	static function drawObject(&$source, &$parents)
+	{
+		$className = get_class($source);
+		$object_index = self::getObjectId($source);
+		$object_id = 'dhph_' . $className . '_' . self::$calls . '_' . $object_index;
 
-    if (self::isRecursiveObject($source, $parents))
-    {
-      self::drawValue('<a href="#' .  $object_id . '" onclick="dumphper_show(this);">' . $className . ' #' . $object_index . '</a>' , 'recursion');
-    }
-    else
-    {
-      $sClass = new ReflectionObject($source);
-      $statics = null;
-      $class = $sClass->getParentClass();
-      $classArray = '';
-      while ( is_object($class) )
-      {
-        $classArray = ' &gt; ' . $class->getName() . $classArray;
-        $class = $class->getParentClass();
-      }
-      
-      echo '<div class="dumphper-container' . (self::$depth <= self::$max_showw_depth ? '' : ' dumphper-closed') . '">';
-      self::drawHeader( '<a id="' .  $object_id . '" name="' .  $object_id . '" class="dumphper-toggler" href="javascript:;" onclick="dumphper_toggle(this);">' . $className . ' #' . $object_index .
-        (count($classArray) ? ' <span class="dumphper-class-def"> ' . $classArray . '</span>' : '' ).
-        '</a>', 'object');
-      echo '<table class="dumphper-table" cellspacing="0" cellpadding="0">';
-      
-      if(!DPHP_USE_ACCESSIBLE)
-        $temp = (array)$source;
+		if (self::isRecursiveObject($source, $parents))
+		{
+			self::drawValue('<a href="#' .  $object_id . '" onclick="dumphper_show(this);">' . $className . ' #' . $object_index . '</a>' , 'recursion');
+		}
+		else
+		{
+			$sClass = new ReflectionObject($source);
+			$statics = null;
+			$class = $sClass->getParentClass();
+			$classArray = '';
+			while ( is_object($class) )
+			{
+				$classArray = ' &gt; ' . $class->getName() . $classArray;
+				$class = $class->getParentClass();
+			}
+			
+			echo '<div class="dumphper-container' . (self::$depth <= self::$max_showw_depth ? '' : ' dumphper-closed') . '">';
+			self::drawHeader( '<a id="' .  $object_id . '" name="' .  $object_id . '" class="dumphper-toggler" href="javascript:;" onclick="dumphper_toggle(this);">' . $className . ' #' . $object_index .
+				(count($classArray) ? ' <span class="dumphper-class-def"> ' . $classArray . '</span>' : '' ).
+				'</a>', 'object');
+			echo '<table class="dumphper-table" cellspacing="0" cellpadding="0">';
+			
+			if(!DPHP_USE_ACCESSIBLE)
+				$temp = (array)$source;
 
-       $class = $sClass;
-      while ( is_object($class) )
-      {
-        $properties = $class->getProperties();
-        if ($class->name != $className && count($properties))
-        {
-          echo '<tr><td colspan = "2">';
-          self::drawValue('<code>inherited from </code>' . $class->name . ':', 'inherited');
-          echo '</td></tr>';
-        }
-        foreach ($properties as &$value)
-        {
-          $declaredClass = $value->getDeclaringClass()->name;
-          if ($class->name == $declaredClass)
-          {
-            echo '<tr><td class="dumphper-key-object">';
-            self::drawValue($value->name/* . ' <span class="dumphper-class-def">' . $declaredClass . '</span>'*/, join('-',Reflection::getModifierNames($value->getModifiers())));
-            echo '</td><td>';
-            if ($value->isPublic()) {
-              $tmp = $value->getValue($source);
-              self::_dump($tmp, $parents);
-            }
-            /** in case of PHP 5.3+ **/
-            elseif (DPHP_USE_ACCESSIBLE)
-            {
-              $value->setAccessible(true);
-              self::_dump($value->getValue($source), $parents);
-            }
-            /** in case of PHP 5.2.x+ **/
-            elseif ($value->isStatic())
-            {
-              if (!$statics) $statics = $class->getStaticProperties();
-              self::_dump($statics[$value->name], $parents);
-            }
-            else
-            {
-              $scope = $value->isPrivate() ? $value->class : '*';
-              self::_dump($temp["\0{$scope}\0{$value->name}"], $parents);
-            }
-            echo '</td></tr>';
-          }
-        }
-        $class = $class->getParentClass();
-      }
-      echo '</table>';
-      echo '</div>';
-    }
-  }
-  
-  static function isRecursiveArray(&$source, &$parents)
-  {
-    if ( count($parents) > 0 )
-    {
-      $uKey = uniqid('array', true);
-      $source[$uKey] = true;
-      foreach ( $parents as &$parrent )
-      {
-        if ( is_array($parrent) && isset($parrent[$uKey]) )
-        {
-          unset($source[$uKey]);
-          return true;
-        }
-      }
-      unset($source[$uKey]);
-    }
-    $parents[] = &$source;
-    return false;
-  }
-  
-  static function isRecursiveObject(&$source, &$parents)
-  {
-    if ( in_array($source, $parents, true) )
-      return true;
-    $parents[] = &$source;
-    return false;
-  }
-  
-  static function drawValue($value, $type)
-  {
-    echo '<span class="dumphper-value dumphper-' . $type . '" title="' . $type . '">' . $value . '</span>';
-  }
-  
-  static function drawHeader($value, $type)
-  {
-    echo '<div class="dumphper-head dumphper-head-' . $type . '" title="' . $type . '">' . $value .'</div>';
-  }
+				$class = $sClass;
+			while ( is_object($class) )
+			{
+				$properties = $class->getProperties();
+				if ($class->name != $className && count($properties))
+				{
+					echo '<tr><td colspan = "2">';
+					self::drawValue('<code>inherited from </code>' . $class->name . ':', 'inherited');
+					echo '</td></tr>';
+				}
+				foreach ($properties as &$value)
+				{
+					$declaredClass = $value->getDeclaringClass()->name;
+					if ($class->name == $declaredClass)
+					{
+						echo '<tr><td class="dumphper-key-object">';
+						self::drawValue($value->name/* . ' <span class="dumphper-class-def">' . $declaredClass . '</span>'*/, join('-',Reflection::getModifierNames($value->getModifiers())));
+						echo '</td><td>';
+						if ($value->isPublic()) {
+							$tmp = $value->getValue($source);
+							self::_dump($tmp, $parents);
+						}
+						/** in case of PHP 5.3+ **/
+						elseif (DPHP_USE_ACCESSIBLE)
+						{
+							$value->setAccessible(true);
+							self::_dump($value->getValue($source), $parents);
+						}
+						/** in case of PHP 5.2.x+ **/
+						elseif ($value->isStatic())
+						{
+							if (!$statics) $statics = $class->getStaticProperties();
+							self::_dump($statics[$value->name], $parents);
+						}
+						else
+						{
+							$scope = $value->isPrivate() ? $value->class : '*';
+							self::_dump($temp["\0{$scope}\0{$value->name}"], $parents);
+						}
+						echo '</td></tr>';
+					}
+				}
+				$class = $class->getParentClass();
+			}
+			echo '</table>';
+			echo '</div>';
+		}
+	}
+	
+	static function isRecursiveArray(&$source, &$parents)
+	{
+		if ( count($parents) > 0 )
+		{
+			$uKey = uniqid('array', true);
+			$source[$uKey] = true;
+			foreach ( $parents as &$parrent )
+			{
+				if ( is_array($parrent) && isset($parrent[$uKey]) )
+				{
+					unset($source[$uKey]);
+					return true;
+				}
+			}
+			unset($source[$uKey]);
+		}
+		$parents[] = &$source;
+		return false;
+	}
+	
+	static function isRecursiveObject(&$source, &$parents)
+	{
+		if ( in_array($source, $parents, true) )
+			return true;
+		$parents[] = &$source;
+		return false;
+	}
+	
+	static function drawValue($value, $type)
+	{
+/*        if ($type == 'string') {
+            echo '<pre>', varDump($value);
+            die;
+        }*/
+		echo '<span class="dumphper-value dumphper-' . $type . '" title="' . $type . '">' . $value . '</span>';
+	}
+	
+	static function drawHeader($value, $type)
+	{
+		echo '<div class="dumphper-head dumphper-head-' . $type . '" title="' . $type . '">' . $value .'</div>';
+	}
 
-  static function drawStyles()
-  {
-    static $displayed = false;
-    if ($displayed) return; //Exit if already shown
-    $displayed = true;
+	static function drawStyles()
+	{
+		static $displayed = false;
+		if ($displayed) return; //Exit if already shown
+		$displayed = true;
 ?><style type="text/css">
 .dumphper { margin: 0 0 3px !important; /*max-width:500px !important;*/}
 .dumphper-container { display: inline-block !important; position: relative !important; }
@@ -289,17 +296,17 @@ class Dumphper
 <script type="text/javascript">
 function dumphper_toggle(obj)
 {
-  obj.parentNode.parentNode.className = (obj.parentNode.parentNode.className == 'dumphper-container' ? 'dumphper-container dumphper-closed' : 'dumphper-container');
+	obj.parentNode.parentNode.className = (obj.parentNode.parentNode.className == 'dumphper-container' ? 'dumphper-container dumphper-closed' : 'dumphper-container');
 }
 function dumphper_show(source_obj)
 {
-  obj = document.getElementById(source_obj.href.match(/[^#]+$/));
-  while(obj = obj.parentNode)
-    if (obj.className == 'dumphper-container dumphper-closed')
-      obj.className = 'dumphper-container';
+	obj = document.getElementById(source_obj.href.match(/[^#]+$/));
+	while(obj = obj.parentNode)
+		if (obj.className == 'dumphper-container dumphper-closed')
+			obj.className = 'dumphper-container';
 }
 </script><?php
-  }
+	}
 }
 
 /** Comment if you don't need this **/
